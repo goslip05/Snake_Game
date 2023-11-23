50cd93f79d7c25bd01a32b1827fd23f9c3ed8f5e
Diego Jimenez <71677761+goslip05@users.noreply.github.com>
2023-11-23 17:10:07 -0500

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import pygame
 import random
 import os
+import sys
 from pygame.locals import *
 from pygame.math import Vector2
 
@@ -22,17 +23,21 @@ pygame.init()
 size = width,height = (720,480)
 print(size[0])
 
-SNAKE_BODY = pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/snakebody.png")),(10,10))
+#SNAKE_BODY = pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/snakebody.png")),(10,10))
+SNAKE_BODY = pygame.transform.scale(pygame.image.load(os.path.join(r"C:\Snake_Game\images\snakebody.png")),(10,10))
 
-APPLE = pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/manzana.png")),(10,10))
+#APPLE = pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/manzana.png")),(10,10))
+APPLE = pygame.transform.scale(pygame.image.load(os.path.join(r"C:\Snake_Game\images\manzana.png")),(10,10))
 
 SNAKE_HEAD = []
 for x in range(1,5):
-    SNAKE_HEAD += [pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/SnakeHead"+str(x)+".png")),(10,10))]
+    #SNAKE_HEAD += [pygame.transform.scale(pygame.image.load(os.path.join(r"/Users/diegojimenez/VideoJuegoUAN/Snake_Game/images/SnakeHead"+str(x)+".png")),(10,10))]
+    SNAKE_HEAD += [pygame.transform.scale(pygame.image.load(os.path.join(r"C:\Snake_Game\images\SnakeHead"+str(x)+".png")),(10,10))]
     #print(x)
 
 
-EAT_SOUND = pygame.mixer.Sound("/Users/diegojimenez/VideoJuegoUAN/Snake_Game/coin.wav")
+#EAT_SOUND = pygame.mixer.Sound("/Users/diegojimenez/VideoJuegoUAN/Snake_Game/coin.wav")
+#EAT_SOUND = pygame.mixer.Sound("C:\Snake_Game\coin.wav")
 
 screen = pygame.display.set_mode(size)
 SCORE_TEXT = pygame.font.SysFont("Russo One",15)
@@ -125,7 +130,114 @@ class Apple:
         
         return False
 
-def main():
+class SceneManager:
+    def __init__(self):
+        self.current_scene = None
+
+    def set_scene(self, new_scene):
+        self.current_scene = new_scene
+
+    def run_scene(self):
+        self.current_scene.run()
+
+class MainMenuScene:
+    def __init__(self, scene_manager):
+        self.scene_manager = scene_manager
+
+    def handle_events(self, events):
+        for event in events:
+            if event.type == QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == KEYDOWN:
+                if event.key == K_RETURN:
+                    scene_manager = SceneManager()
+                    print('enter')
+                    scene_manager.set_scene(GameScene(scene_manager))
+
+    def draw(self, screen):
+        # Limpia la pantalla
+        screen.fill(BLACK)
+
+        # Dibuja el texto del menú
+        font = pygame.font.SysFont("Russo One", 50)
+        text_play = font.render("Jugar", True, WHITE)
+
+        play_rect = text_play.get_rect(center=(width / 2, height / 2 - 50))
+        screen.blit(text_play, play_rect)
+
+        pygame.display.flip()
+
+    def run(self):
+        while True:
+            events = pygame.event.get()
+            self.handle_events(events)
+            self.draw(screen)
+class GameScene:
+    def __init__(self, scene_manager):
+        self.scene_manager = scene_manager
+        self.snake = Snake()
+        self.apple = Apple()
+
+    def handle_events(self, events):
+        for event in  events:
+            if event.type == pygame.QUIT:
+                running = False
+                
+            #metodo para mover serpiete hacia arriba
+            if event.type == pygame.KEYDOWN and self.snake.direccion.y != 10:
+                if event.key == pygame.K_UP:
+                    self.snake.move_up()
+                    
+            #metodo para mover serpiete hacia abajo
+            if event.type == pygame.KEYDOWN and self.snake.direccion.y != -10:
+                if event.key == pygame.K_DOWN:
+                    self.snake.move_down()
+                    
+            #metodo para mover serpiete hacia la izquierda
+            if event.type == pygame.KEYDOWN and self.snake.direccion.x != 10:
+                if event.key == pygame.K_LEFT:
+                    self.snake.move_left()
+                    
+            #metodo para mover serpiete hacia la derecha
+            if event.type == pygame.KEYDOWN and self.snake.direccion.x != -10:
+                if event.key == pygame.K_RIGHT:
+                    print('derecha')
+                    self.snake.move_right()
+
+    def draw(self, screen):
+        # Limpia la pantalla
+        screen.fill((175, 215, 70))
+        score = 0
+        # Dibuja el juego
+        self.snake.draw()
+        self.apple.draw()
+        self.snake.move()
+
+        if self.snake.die():
+            quit()
+
+        if self.apple.check_collision(self.snake):
+            #EAT_SOUND.play()
+            score+=1
+
+        text = SCORE_TEXT.render("Score: {}".format(score), 1, WHITE)
+        screen.blit(text, (width - text.get_width() - 10, 10))
+
+        pygame.display.update()
+
+    def run(self):
+        fps = pygame.time.Clock()
+        
+        while True:
+            fps.tick(5)
+            #print("Game Scene Running")
+            events = pygame.event.get()
+            self.handle_events(events)
+            self.draw(screen)
+            
+            
+"""def main():
     
     snake = Snake()
     apple = Apple()
@@ -171,11 +283,23 @@ def main():
             
         if apple.check_collision(snake):
             score+=1
-            EAT_SOUND.play()
+            #EAT_SOUND.play()
             
         text = SCORE_TEXT.render("Score: {}".format(score),1,WHITE)
         screen.blit(text,(size[0]-text.get_width()-10,10))
-        pygame.display.update()
+        pygame.display.update()"""
+        
+def main():
+    pygame.init()
+
+    size = width, height = (720, 480)
+    screen = pygame.display.set_mode(size)
+
+    scene_manager = SceneManager()
+    scene_manager.set_scene(GameScene(scene_manager))
+
+    while True:
+        scene_manager.run_scene()
 
 main()
 
